5abfd24ceae5e513ab73d8749c570bdca1918ccb
MrBroccoli128 <jonathan.linder@stud.htwchur.ch>
2019-09-29 18:48:57 +0200

    Created projectidea

--- HelloW.py
+++ HelloW.py
@@ -1,3 +1,16 @@
-print("Hello World")
+from flask import Flask
+from flask import render_template
 
-asdmfasd
+app = Flask("Hello World")
+
+@app.route('/hello')
+def hello_world():
+    name = "Jonathan2"
+    return render_template('index.html', name=name, ha="Vogel")
+
+@app.route("/test")
+def test():
+    return "success"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
